cbef1fb2195deb07c0c3c6c0bcae53ec0a37fa9b
khabarkasafar <143529854+khabarkasafar@users.noreply.github.com>
2023-08-31 18:37:53 +0530

    Update crypto.js

--- src/config/crypto.js
+++ src/config/crypto.js
@@ -5,6 +5,5 @@ const generateSecretKey = () => {
   return crypto.randomBytes(32).toString("hex");
 };
 
-const SecretKey = generateSecretKey();
-console.log(SecretKey);
+const SecretKey ="2809a95eedde5863d8e8e3bea5205cd62d290b10a3769afee677b8754a4d05b7"
 module.exports = SecretKey;
